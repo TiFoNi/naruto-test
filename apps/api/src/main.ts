dc3ba337b525b90a7e2312f132a56bad69ffcc2d
Illia Ushakow <iitbgtifoni@gmail.com>
2026-09-25 09:13:52 +0200

    feat: fix optima

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata'
 import { NestFactory } from '@nestjs/core'
 import type { NestExpressApplication } from '@nestjs/platform-express'
+import compression from 'compression'
 import { raw } from 'express'
 import { AppModule } from './app.module'
 
@@ -10,6 +11,7 @@ async function bootstrap() {
   const port = Number(process.env.PORT ?? 4000)
   const app = await NestFactory.create<NestExpressApplication>(AppModule)
   app.set('trust proxy', 1)
+  app.use(compression())
   app.use(raw({ type: 'multipart/form-data', limit: '40mb' }))
   app.enableCors({ origin: origins, credentials: true })
   await app.listen(port, '0.0.0.0')
